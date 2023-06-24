1e6aa9a8786077f18952e8b1f54d49e22bee06f5
dilawar10 <dilawar@hnhtechsolutions.com>
2023-06-24 15:51:18 +0500

    done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import {Routes,Route,BrowserRouter} from 'react-router-dom'
+import Home from './pages/Home';
+import StickyHeader from './components/Header'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle'
+import Footer from './components/Footer';
+import './components/Style.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <>
+   <div className='App'>
+    <BrowserRouter>
+     <StickyHeader/>
+       <Routes>
+         <Route path='/' element={<Home/>}/>
+       </Routes>
+       <Footer/>
+    </BrowserRouter>
+
     </div>
+   </>
   );
 }
 
