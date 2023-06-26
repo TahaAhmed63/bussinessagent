19aff04db7c0cd970328ffe952df09b0bc9bb824
dilawar10 <dilawar@hnhtechsolutions.com>
2023-06-26 14:02:39 +0500

    'sectiondone'

--- src/pages/ImportExport/ImportExport.js
+++ src/pages/ImportExport/ImportExport.js
@@ -2,10 +2,12 @@ import React from 'react'
 import Banner from './Banner'
 import './ImportExport.css'
 import Section3 from './Section3'
+import Section2 from './Section2'
 const ImportExport = () => {
   return (
     <div>
         <Banner/>
+        <Section2/>
         <Section3/>
     </div>
   )
