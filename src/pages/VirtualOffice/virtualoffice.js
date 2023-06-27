a120ff76535cbb56eeb95e760362028ca87acf79
dilawar10 <dilawar@hnhtechsolutions.com>
2023-06-27 20:41:48 +0500

    nhjcks

--- src/pages/VirtualOffice/virtualoffice.js
+++ src/pages/VirtualOffice/virtualoffice.js
@@ -8,13 +8,13 @@ import Section5 from './Section5'
 import Section6 from './Section6'
 import Section7 from './Section7'
 import Navbar from '../../MainComponent/Navbar'
-import logo from '../../assets/Logo7.png'
+import Logo from '../../assets/Logo7.png'
 
 
 const Virtualoffice = () => {
     return (
        <div className='text-white'>
-          <Navbar logo={logo} linkcolor="white" />
+          <Navbar logo={Logo} linkcolor="white" />
       <Section1 />
       <Section2 />
       <Section3 />
