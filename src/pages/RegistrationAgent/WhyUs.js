19c6fb48e667d807ca62f596f78068dea4d326e2
tahaahmed12 <tahaahmed@hnhtechsolutions.com>
2023-06-27 12:30:31 +0500

    done 3rd page

--- src/pages/RegistrationAgent/WhyUs.js
+++ src/pages/RegistrationAgent/WhyUs.js
@@ -11,7 +11,9 @@ import myblackimg from './../../assets/Group 516 (1).png'
                 Join hundreds of successful organizations
                 </h1>
                 <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
-    <img src={myblackimg} />
+  <div >
+
+  </div>
             </div>
 
 
