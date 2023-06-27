501eb0ea4d1b5d3333a14e842bd7ba5b99b33bc0
tahaahmed12 <tahaahmed@hnhtechsolutions.com>
2023-06-27 15:35:59 +0500

    sasa

--- src/pages/RegistrationAgent/WhyUs.js
+++ src/pages/RegistrationAgent/WhyUs.js
@@ -6,7 +6,7 @@ import myblackimg from './../../assets/Group 516 (1).png'
 <section className='main-us'>
     <div className='container'>
         <div className='row '>
-            <div col-lg-12 col-xxlg-12 col-md-12 col-sm-12>
+            <div className='col-lg-12 col-xxlg-12 col-md-12 col-sm-12'>
                 <h1>
                 Join hundreds of successful organizations
                 </h1>
